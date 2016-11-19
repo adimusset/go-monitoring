d5b7faef38d884205998f59420b4d4f963e24a77
adimusset <adrien@veyo-care.com>
2016-11-19 18:05:44 +0100

    improve the layout and notify of past alerts

--- console.go
+++ console.go
@@ -1,72 +1,117 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
-	"os"
 	"sort"
 	"time"
 )
 
-const refreshingTimeInSeconds = 3
+const infoRequest = "request"
+const infoSection = "section"
+
+const refreshingTimeInSeconds = 10
 
 var layout, _ = template.New("console").Parse(`
 {{.Time}}
-Most visited section: {{.Section}} : {{.Times}} times
-Last alerts: {{.Alerts}}
+Most visited section: {{.Section}} - {{.TimesSection}}
+Most done request: {{.Request}} - {{.TimesRequest}}{{if .Alerts}}
+Recent alerts: {{.Alerts}}{{end}}
 ------`)
 
-func printOutput(t, section string, times int, alerts string) error {
+func printOutput(t time.Time, infos map[string]Counts, alerts []Alert) error {
+	countsSection, _ := infos[infoSection]
+	countsRequest, _ := infos[infoRequest]
+	hour := t.Local().Format("15:04:05")
 	input := struct {
-		Time    string
-		Section string
-		Times   string
-		Alerts  string
+		Alerts       string
+		Time         string
+		TimesSection string
+		TimesRequest string
+		Section      string
+		Request      string
 	}{
-		Time:    t,
-		Section: section,
-		Times:   fmt.Sprintf("%d", times),
-		Alerts:  alerts,
+		Time:         hour,
+		Section:      countsSection[0].value,
+		Request:      countsRequest[0].value,
+		TimesSection: fmt.Sprintf("%d", countsSection[0].n), //always 1 element after method sortTable
+		TimesRequest: fmt.Sprintf("%d", countsRequest[0].n),
+		Alerts:       alertsToString(alerts),
 	}
-	return layout.Execute(os.Stdout, input)
+	b := new(bytes.Buffer)
+	err := layout.Execute(b, input)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(b.Bytes()))
+	return nil
 }
 
 type Console struct {
 	refresher    *time.Ticker
-	requests     *Storage
 	alerts       chan Alert
 	recentAlerts []Alert
+	requests     *Storage //could add more
+	sections     *Storage
 }
 
-func NewConsole(requests *Storage, alerts chan Alert) *Console {
+func NewConsole(requests *Storage, sections *Storage, alerts chan Alert) *Console {
 	t := time.NewTicker(time.Duration(refreshingTimeInSeconds) * time.Second)
 	return &Console{
 		refresher:    t,
-		requests:     requests,
 		alerts:       alerts,
 		recentAlerts: []Alert{},
+		requests:     requests,
+		sections:     sections,
 	}
 }
 
 func (c *Console) Run() {
 	go c.WatchAlerts()
 	for range c.refresher.C {
-		counts := c.requests.GetCounts()
-		sort.Sort(counts)
-		// need to add intelligence
+		infos := make(map[string]Counts)
+		infos[infoRequest] = c.requests.GetCounts()
+		infos[infoSection] = c.sections.GetCounts()
+		sortTable(infos)
 
-		t := time.Now().Format("15:04:05")
-		if len(counts) == 0 {
-			printOutput(t, "none", 0, "")
-			continue
+		t := time.Now()
+
+		err := printOutput(t, infos, c.recentAlerts)
+		if err != nil {
+			fmt.Println("Display error, ", err.Error())
 		}
-		printOutput(t, counts[0].value, counts[0].n, "")
+	}
+}
+
+func sortTable(in map[string]Counts) {
+	for key, counts := range in {
+		if len(counts) == 0 {
+			in[key] = Counts{Count{value: "none", n: 0}}
+		}
+		sort.Sort(counts)
 	}
 }
 
 func (c *Console) WatchAlerts() {
 	for alert := range c.alerts {
-		fmt.Println(alert)
+		hour := alert.Date.Local().Format("15:04:05")
+		s := ""
+		if alert.Up {
+			s = fmt.Sprintf("High traffic generated an alert - hits = %d, triggered at %s", alert.Average, hour)
+		} else {
+			s = fmt.Sprintf("Traffic back to normal - hits = %d, triggered at %s", alert.Average, hour)
+		}
+		fmt.Println(s)
+
 		c.recentAlerts = append(c.recentAlerts, alert)
 	}
+}
+
+func alertsToString(recentAlerts []Alert) string {
+	result := ""
+	for _, alert := range recentAlerts {
+		result = result + alert.String() + "--"
+	}
+	return result
 }
