19078e097d5d899996b1bb281740abf802a1cf36
adimusset <adrien@veyo-care.com>
2016-11-19 18:03:39 +0100

    use a buffered channel for the input

--- main.go
+++ main.go
@@ -5,17 +5,22 @@ import (
 )
 
 func main() {
-	input := make(chan Object)
+	input := make(chan Object, 100)
 	go logTest(input)
 
+	start(input)
+}
+
+func start(input chan Object) {
 	requests := NewStorage()
+	sections := NewStorage()
 
 	alerts := make(chan Alert)
 
 	consumer := NewConsumer(input)
-	console := NewConsole(requests, alerts)
-	statsReporter := NewStatisticsReporter(requests)
-	averageAlerter := NewAverageAlerter(10, alerts)
+	console := NewConsole(requests, sections, alerts)
+	statsReporter := NewStatisticsReporter(requests, sections)
+	averageAlerter := NewAverageAlerter(10, 15, alerts)
 
 	consumer.Subscribe(statsReporter)
 	consumer.Subscribe(averageAlerter)
@@ -28,10 +33,14 @@ func main() {
 func logTest(output chan Object) {
 	for k := 0; k < 10; k++ {
 		time.Sleep(2 * time.Second)
-		output <- Object{RequestLine: "/section"}
+		output <- Object{RequestLine: "GET /section/page", Date: time.Now()}
+		output <- Object{RequestLine: "GET /section2/page", Date: time.Now()}
+		output <- Object{RequestLine: "GET /section2/page", Date: time.Now()}
+		output <- Object{RequestLine: "GET /section2/page2", Date: time.Now()}
 	}
+	time.Sleep(30 * time.Second)
 	for {
 		time.Sleep(time.Second)
-		output <- Object{RequestLine: "/new"}
+		output <- Object{RequestLine: "POST /new/image", Date: time.Now()}
 	}
 }
